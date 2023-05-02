6402c2057f6d45fb47e17bc8d21e78ffdee50e6b
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-05-02 03:01:11 +0200

    show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.app')
+
 @section('content')
-    edit
+    <div class="container">
+        <div class="d-flex align-items">
+            <div class="me-auto">
+                <h1>{{ $project->title }}</h1>
+                <p>{{ $project->slug }}</p>
+            </div>
+
+
+            <div>
+                <a class="btn btn-sm btn-secondary" href="{{ route('projects.edit', $project) }}">Modifica</a>
+            </div>
+
+        </div>
+    </div>
+    <div class="container">
+        <p>
+            {{ $project->description }}
+        </p>
+    </div>
 @endsection
